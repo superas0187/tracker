f3aa99bca59a9eb395ade488770d3a242820460a
DNA talent <dna0187@gmail.com>
2021-06-06 17:14:31 +0300

    Added h1 tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import Details from './components/Details/styles'; 
+import Details from './components/Details'; 
 
 import useStyles from './styles';
 
