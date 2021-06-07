113bbfe0d65e923f1e3a3d0f8d3260adaff453c6
DNA talent <dna0187@gmail.com>
2021-06-07 15:59:07 +0300

    Added h1 tag

--- src/context/context.js
+++ src/context/context.js
@@ -9,13 +9,9 @@ export const ExpenseTrackerContext = createContext(initialState);
 export const Provider = ({ children }) => {
      const [transactions, dispatch] = useReducer(contextReducer, initialState );
 
-     const deleteTransaction = (id) => {
-         dispatch({ type: 'DELETE_TRANSACTION', payload: id});
-     }
-
-     const addTransaction = (transaction) => {
-         dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
-     }
+     const deleteTransaction = (id) => dispatch({ type: 'DELETE_TRANSACTION', payload: id});
+     const addTransaction = (transaction) => dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
+     
     return(
         <ExpenseTrackerContext.Provider value={{ deleteTransaction, addTransaction}}>
             {children}
