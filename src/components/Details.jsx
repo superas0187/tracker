3763cabdbef1618f56f2667833daa71e4c83fcfb
DNA talent <dna0187@gmail.com>
2021-06-07 23:42:03 +0300

    Added h1 tag

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 
 import { Doughnut } from 'react-chartjs-2';
-import useTransactions from '../../useTransactions';
+import useTransactions from '../useTransactions';
 
 import  useStyles from './Styles';
 
