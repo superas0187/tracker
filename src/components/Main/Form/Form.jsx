495f5b8899afac4ee1107a461afb3bd1cabea25b
DNA talent <dna0187@gmail.com>
2021-06-07 22:30:58 +0300

    Added h1 tag

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -3,15 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 import { ExpenseTrackerContext } from '../../../context/context';
 import useStyles from './styles';
+import formatDate from '../../../utils/formatDate';
 
-import { incomeCategories, expenseCategories} from '../../../contants/categories';
+import { incomeCategories, expenseCategories} from '../../../constants/categories';
 
 
 const initialState = {
     amount: '',
     category: '',
     type: 'Income',
-    date: new Date(),
+    date: formatDate(new Date()),
 }
 
 const Form = () => {
@@ -57,7 +58,7 @@ const Form = () => {
                 <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} /> 
             </Grid>
             <Grid item xs={6}>
-                <TextField type="data" label="Data" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })}/> 
+                <TextField type="data" label="Data" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate( e.target.value) })}/> 
             </Grid>
             <Button className={classes.button} variavt="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
