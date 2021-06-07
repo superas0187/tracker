091d1330c150bc19100107273df8de5c43a4828c
DNA talent <dna0187@gmail.com>
2021-06-07 17:32:20 +0300

    Added h1 tag

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemSecondaryAction, IconButton, Slide, ListItemText } from '@material-ui/core';
 import{ Delete, MoneyOff } from '@material-ui/icons';
 
-import { ExpenseTrackerContext } from '../../context/context';
+import { ExpenseTrackerContext } from '../../../context/context';
 
 
 
