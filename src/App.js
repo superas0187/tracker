c6b16195e94e531145e030355706a66d3977aae5
DNA talent <dna0187@gmail.com>
2021-06-08 22:39:07 +0300

    Added h1 tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
 import Details from './components/Details'; 
 import Main from './components/Main/Main';
 
@@ -21,6 +21,10 @@ const App = () => {
                     <Details title="Expense"/>
                 </Grid>
             </Grid>
+            <PushToTalkButtonContainer>
+                <PushToTalkButton />
+                <ErrorPanel />
+            </PushToTalkButtonContainer>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from './context/context';
+import { SpeechProviser } from '@speechly/react-client';
 import App from './App';
 import './index.css';
 
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProviser appId="d5e583c7-1a9b-4649-aaee-0eedf8109e95" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProviser>,
     document.getElementById('root')
 );
