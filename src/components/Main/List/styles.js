faa4eefb261f8835478206387876d09242f65ba2
DNA talent <dna0187@gmail.com>
2021-06-10 18:46:26 +0300

    Added h1 tag

--- src/components/Main/List/styles.js
+++ src/components/Main/List/styles.js
@@ -1,10 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { red, green } from '@material-ui/core/colors';
 
+
 export default makeStyles((theme) => ({
   avatarIncome: {
-    color: '#fff',
+    color: '#red',
     backgroundColor: green[500],
+    
   },
   avatarExpense: {
     color: theme.palette.getContrastText(red[500]),
