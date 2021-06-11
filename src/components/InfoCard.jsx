bd84920853dc3111f4d1514ff8ea08a438f10964
DNA talent <dna0187@gmail.com>
2021-06-11 14:54:13 +0300

    Added h1 tag

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -6,7 +6,7 @@ const isIncome = Math.round(Math.random());
 const InfoCard = () => {
     return(
         <div style={{ textAlign: 'center', padding: '0 10%' }}>
-            Try saying: <br />
+            <h2>Try saying:</h2> 
             Add {isIncome ? 'Income ' : 'Expense '}
             for { isIncome ? '$100 ' : '$50 '}
             in Category {isIncome ? 'Business ' : 'House '}
